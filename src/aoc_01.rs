e6c5a692cfaa323352e9a77399a3fbac2bf5d190
Dariusz D <darekduda21@gmail.com>
2024-12-01 20:03:38 -0500

    feat(aoc_01): add solution for part 2
    Signed-off-by: Dariusz D <darekduda21@gmail.com>

--- src/aoc_01.rs
+++ src/aoc_01.rs
@@ -1,10 +1,10 @@
 #[path = "utils.rs"]
 mod utils;
 
-pub fn solve_part_1(filepath: &str) -> i64 {
+fn load_vectors(filepath: &str) -> (Vec<u64>, Vec<u64>) {
     let lines = utils::read_file(filepath);
-    let mut left_vec: Vec<i64> = Vec::new();
-    let mut right_vec: Vec<i64> = Vec::new();
+    let mut left_vec: Vec<u64> = Vec::new();
+    let mut right_vec: Vec<u64> = Vec::new();
     for line in lines {
         let splitted: Vec<_> = line.split("   ").collect();
         if splitted.len() == 2 {
@@ -13,12 +13,29 @@ pub fn solve_part_1(filepath: &str) -> i64 {
             right_vec.push(right.parse().unwrap());
         }
     }
+    (left_vec, right_vec)
+}
+
+pub fn solve_part_1(filepath: &str) -> u64 {
+    let (mut left_vec, mut right_vec) = load_vectors(filepath);
     left_vec.sort();
     right_vec.sort();
 
     std::iter::zip(left_vec, right_vec)
-        .map(|(l, r)| (r - l).abs())
+        // implement abs without using i64 to store
+        .map(|(l, r)| if r > l { r - l } else { l - r })
         .sum()
+}
+
+pub fn solve_part_2(filepath: &str) -> u64 {
+    let (left_vec, right_vec) = load_vectors(filepath);
+    let mut res = 0;
+
+    for num in left_vec {
+        res += num * (right_vec.iter().filter(|&v| *v == num).count() as u64);
+    }
+
+    res
 }
 
 #[cfg(test)]
@@ -26,7 +43,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_example_case() {
+    fn test_example_part1() {
         assert_eq!(solve_part_1("input_01_test"), 11);
+    }
+
+    #[test]
+    fn test_example_part2() {
+        assert_eq!(solve_part_2("input_01_test"), 31);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,6 @@ mod aoc_01;
 fn main() {
     let res = aoc_01::solve_part_1("input_01");
     println!("Result: {}", res);
+    let res = aoc_01::solve_part_2("input_01");
+    println!("Result: {}", res);
 }
