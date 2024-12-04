8dcad2d28a11b442bf7d480253d8f9f1032b1a3a
Dariusz D <darekduda21@gmail.com>
2024-12-03 19:27:46 -0500

    feat(aoc_03): add solution for part 2
    Signed-off-by: Dariusz D <darekduda21@gmail.com>

--- src/aoc_03.rs
+++ src/aoc_03.rs
@@ -37,16 +37,72 @@ pub fn solve_part_1(filepath: &str) -> u64 {
     let mut res = 0;
 
     for (_op, first, second) in operations {
-        println!("{} * {}", first, second);
         res += first * second;
     }
 
     res
 }
 
+fn load_operations_part2(filepath: &str) -> Vec<(Operation, u64, u64)> {
+    let lines = utils::read_file(filepath);
+    let mut operations = Vec::new();
+
+    let mut enabled = true;
+    for line in lines {
+        let mut curr_idx = 0;
+        let mut curr_buff = line.clone();
+        while curr_idx <= line.len() {
+            curr_buff = curr_buff[curr_idx..].to_string();
+            if enabled {
+                let dont = curr_buff.find("don't()");
+                match dont {
+                    Some(idx) => {
+                        if enabled {
+                            let val = &curr_buff[..idx];
+                            println!("{}", val);
+                            for (first, second) in find_values(&curr_buff[..idx]) {
+                                operations.push((Operation::Multiply, first, second));
+                            }
+                        }
+                        enabled = false;
+                        curr_idx = idx + 1;
+                        continue;
+                    }
+                    None => {
+                        if enabled {
+                            for (first, second) in find_values(&curr_buff) {
+                                operations.push((Operation::Multiply, first, second));
+                            }
+                            break;
+                        }
+                    }
+                }
+            } else {
+                let do_match = curr_buff.find("do()");
+                match do_match {
+                    Some(idx) => {
+                        let val = &curr_buff[idx..];
+                        println!("{}", val);
+                        enabled = true;
+                        curr_idx = idx + 1;
+                    }
+                    None => break,
+                }
+            }
+        }
+    }
+
+    operations
+}
+
 pub fn solve_part_2(filepath: &str) -> u64 {
-    let operations = load_operations(filepath);
+    let operations = load_operations_part2(filepath);
     let mut res = 0;
+
+    for (_op, first, second) in operations {
+        println!("{} * {}", first, second);
+        res += first * second;
+    }
 
     res
 }
