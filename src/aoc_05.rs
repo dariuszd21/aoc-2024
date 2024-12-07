8748480511d5355e4c5c3e49b21d21e8af2780f0
Dariusz D <darekduda21@gmail.com>
2024-12-06 19:41:24 -0500

    feat(aoc_05): add soultion for part 2
    Signed-off-by: Dariusz D <darekduda21@gmail.com>

--- src/aoc_05.rs
+++ src/aoc_05.rs
@@ -59,7 +59,7 @@ fn check_print_order(print_order: &Vec<u64>, rules: &HashMap<u64, HashSet<u64>>)
             let prev_val = print_order[prev];
             if let Some(ruleset) = rules.get(&val) {
                 if ruleset.contains(&prev_val) {
-                    println!("Rule not followed: {} before {}", prev, val);
+                    println!("Rule not followed: {} before {}", prev_val, val);
                     return false;
                 }
             }
@@ -67,6 +67,36 @@ fn check_print_order(print_order: &Vec<u64>, rules: &HashMap<u64, HashSet<u64>>)
     }
 
     true
+}
+
+fn fix_ordering(print_order: &Vec<u64>, rules: &HashMap<u64, HashSet<u64>>) -> Vec<u64> {
+    let mut correct_vec = print_order.clone();
+
+    while !check_print_order(&correct_vec, rules) {
+        let curr_vec = correct_vec.clone();
+        println!("Current vec: {:?}", correct_vec);
+        for i in 0..curr_vec.len() {
+            let val = curr_vec[i];
+            let mut stop = false;
+
+            for prev in 0..i {
+                let prev_val = curr_vec[prev];
+                if let Some(ruleset) = rules.get(&val) {
+                    if ruleset.contains(&prev_val) {
+                        correct_vec.remove(i);
+                        correct_vec.insert(prev, val);
+                        stop = true;
+                        break;
+                    }
+                }
+            }
+            if stop {
+                break;
+            }
+        }
+    }
+
+    correct_vec
 }
 
 pub fn solve_part_1(filepath: &str) -> u64 {
@@ -93,6 +123,20 @@ pub fn solve_part_2(filepath: &str) -> u64 {
     let lines = load_lines(filepath);
     let mut res = 0;
 
+    let rules = load_rules(&lines);
+    let print_orders = load_print_orders(&lines);
+
+    println!("Rules: {:?}", rules);
+    println!("Print orders: {:?}", print_orders);
+
+    for print_order in print_orders {
+        if !check_print_order(&print_order, &rules) {
+            let new_values = fix_ordering(&print_order, &rules);
+            let val = new_values[new_values.len() / 2];
+            res += val;
+        }
+    }
+
     res
 }
 
@@ -107,6 +151,6 @@ mod tests {
 
     #[test]
     fn test_example_part2() {
-        assert_eq!(solve_part_2("input_05_test"), 9);
+        assert_eq!(solve_part_2("input_05_test"), 123);
     }
 }
