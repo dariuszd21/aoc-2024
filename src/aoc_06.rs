e0d4226cc46fa4c10ddf88cf37198e9d1d445fce
Dariusz D <darekduda21@gmail.com>
2024-12-07 01:16:25 -0500

    feat(aoc_06): add soultion for part 2
    Signed-off-by: Dariusz D <darekduda21@gmail.com>

--- src/aoc_06.rs
+++ src/aoc_06.rs
@@ -1,6 +1,9 @@
 #[path = "utils.rs"]
 mod utils;
 
+use std::collections::HashSet;
+
+#[derive(Clone, PartialEq, Debug)]
 enum Direction {
     North,
     East,
@@ -10,7 +13,7 @@ enum Direction {
 
 #[derive(Clone)]
 enum Tile {
-    Visited,
+    Visited(Vec<Direction>),
     Empty,
     Obstacle,
     Guard,
@@ -68,9 +71,18 @@ fn can_move(
 ) -> bool {
     match guard_direction {
         Direction::North => guard_i > 0,
-        Direction::East => guard_j > 0,
-        Direction::West => guard_j < map_y - 1,
+        Direction::West => guard_j > 0,
+        Direction::East => guard_j < map_y - 1,
         Direction::South => guard_i < map_x - 1,
+    }
+}
+
+fn rotate_guard(guard_direction: Direction) -> Direction {
+    match guard_direction {
+        Direction::North => Direction::East,
+        Direction::East => Direction::South,
+        Direction::West => Direction::North,
+        Direction::South => Direction::West,
     }
 }
 
@@ -83,10 +95,10 @@ fn simulate_movement(map: &Vec<Vec<Tile>>) -> Vec<Vec<Tile>> {
 
     if let Some((guard_i, guard_j)) = find_guard(&map) {
         let mut map = map.clone();
+        map[guard_i][guard_j] = Tile::Empty;
         let mut guard_direction = Direction::North;
         let (mut guard_pos_i, mut guard_pos_j) = (guard_i, guard_j);
 
-        map[guard_i][guard_j] = Tile::Empty;
         while can_move(
             guard_pos_i,
             guard_pos_j,
@@ -94,7 +106,7 @@ fn simulate_movement(map: &Vec<Vec<Tile>>) -> Vec<Vec<Tile>> {
             num_cols,
             &guard_direction,
         ) {
-            explored_map[guard_pos_i][guard_pos_j] = Tile::Visited;
+            explored_map[guard_pos_i][guard_pos_j] = Tile::Visited(vec![guard_direction.clone()]);
             match guard_direction {
                 Direction::North => {
                     let new_guard_i = guard_pos_i - 1;
@@ -102,27 +114,27 @@ fn simulate_movement(map: &Vec<Vec<Tile>>) -> Vec<Vec<Tile>> {
                         Tile::Empty => {
                             guard_pos_i = new_guard_i;
                         }
-                        Tile::Obstacle => guard_direction = Direction::West,
+                        Tile::Obstacle => guard_direction = rotate_guard(guard_direction),
                         _ => (),
                     }
                 }
                 Direction::East => {
-                    let new_guard_j = guard_pos_j - 1;
-                    match map[guard_pos_i][new_guard_j] {
-                        Tile::Empty => {
-                            guard_pos_j = new_guard_j;
-                        }
-                        Tile::Obstacle => guard_direction = Direction::North,
-                        _ => (),
-                    }
-                }
-                Direction::West => {
                     let new_guard_j = guard_pos_j + 1;
                     match map[guard_pos_i][new_guard_j] {
                         Tile::Empty => {
                             guard_pos_j = new_guard_j;
                         }
-                        Tile::Obstacle => guard_direction = Direction::South,
+                        Tile::Obstacle => guard_direction = rotate_guard(guard_direction),
+                        _ => (),
+                    }
+                }
+                Direction::West => {
+                    let new_guard_j = guard_pos_j - 1;
+                    match map[guard_pos_i][new_guard_j] {
+                        Tile::Empty => {
+                            guard_pos_j = new_guard_j;
+                        }
+                        Tile::Obstacle => guard_direction = rotate_guard(guard_direction),
                         _ => (),
                     }
                 }
@@ -132,16 +144,155 @@ fn simulate_movement(map: &Vec<Vec<Tile>>) -> Vec<Vec<Tile>> {
                         Tile::Empty => {
                             guard_pos_i = new_guard_i;
                         }
-                        Tile::Obstacle => guard_direction = Direction::East,
+                        Tile::Obstacle => guard_direction = rotate_guard(guard_direction),
                         _ => (),
                     }
                 }
             }
         }
-        explored_map[guard_pos_i][guard_pos_j] = Tile::Visited;
+        explored_map[guard_pos_i][guard_pos_j] = Tile::Visited(vec![guard_direction.clone()]);
     }
 
     explored_map
+}
+fn detect_loops(map: &Vec<Vec<Tile>>) -> bool {
+    let mut explored_map = map.clone();
+
+    let num_rows = map.len();
+    let num_cols = map[0].len();
+
+    if let Some((guard_i, guard_j)) = find_guard(&map) {
+        explored_map[guard_i][guard_j] = Tile::Empty;
+        let mut guard_direction = Direction::North;
+        let (mut guard_pos_i, mut guard_pos_j) = (guard_i, guard_j);
+
+        while can_move(
+            guard_pos_i,
+            guard_pos_j,
+            num_rows,
+            num_cols,
+            &guard_direction,
+        ) {
+            match &mut explored_map[guard_pos_i][guard_pos_j] {
+                Tile::Visited(vec) => vec.push(guard_direction.clone()),
+                Tile::Empty => {
+                    explored_map[guard_pos_i][guard_pos_j] =
+                        Tile::Visited(vec![guard_direction.clone()]);
+                }
+                Tile::Obstacle => todo!(),
+                Tile::Guard => todo!(),
+            }
+            match guard_direction {
+                Direction::North => {
+                    let new_guard_i = guard_pos_i - 1;
+                    match &explored_map[new_guard_i][guard_pos_j] {
+                        Tile::Empty => {
+                            guard_pos_i = new_guard_i;
+                        }
+                        Tile::Obstacle => guard_direction = rotate_guard(guard_direction),
+                        Tile::Visited(directions) => {
+                            if directions.contains(&guard_direction) {
+                                return true;
+                            }
+                            guard_pos_i = new_guard_i;
+                        }
+                        _ => (),
+                    }
+                }
+                Direction::East => {
+                    let new_guard_j = guard_pos_j + 1;
+                    match &explored_map[guard_pos_i][new_guard_j] {
+                        Tile::Empty => {
+                            guard_pos_j = new_guard_j;
+                        }
+                        Tile::Obstacle => guard_direction = rotate_guard(guard_direction),
+                        Tile::Visited(directions) => {
+                            if directions.contains(&guard_direction) {
+                                return true;
+                            }
+                            guard_pos_j = new_guard_j;
+                        }
+                        _ => (),
+                    }
+                }
+                Direction::West => {
+                    let new_guard_j = guard_pos_j - 1;
+                    match &explored_map[guard_pos_i][new_guard_j] {
+                        Tile::Empty => {
+                            guard_pos_j = new_guard_j;
+                        }
+                        Tile::Obstacle => guard_direction = rotate_guard(guard_direction),
+                        Tile::Visited(directions) => {
+                            if directions.contains(&guard_direction) {
+                                return true;
+                            }
+                            guard_pos_j = new_guard_j;
+                        }
+                        _ => (),
+                    }
+                }
+                Direction::South => {
+                    let new_guard_i = guard_pos_i + 1;
+                    match &explored_map[new_guard_i][guard_pos_j] {
+                        Tile::Empty => {
+                            guard_pos_i = new_guard_i;
+                        }
+                        Tile::Obstacle => guard_direction = rotate_guard(guard_direction),
+                        Tile::Visited(directions) => {
+                            if directions.contains(&guard_direction) {
+                                return true;
+                            }
+                            guard_pos_i = new_guard_i;
+                        }
+                        _ => (),
+                    }
+                }
+            }
+
+            // we are back at start
+            if (guard_i == guard_pos_i)
+                && (guard_j == guard_pos_j)
+                && guard_direction == Direction::North
+            {
+                return true;
+            }
+        }
+        explored_map[guard_pos_i][guard_pos_j] = Tile::Visited(vec![guard_direction.clone()]);
+    }
+
+    false
+}
+
+fn find_loops(map: &Vec<Vec<Tile>>) -> u64 {
+    let mut loops = 0;
+
+    let num_rows = map.len();
+    let num_cols = map[0].len();
+    println!("Map size {} x {}", num_rows, num_cols);
+
+    let mut explored_map = map.clone();
+    explored_map[9][7] = Tile::Obstacle;
+    let res = detect_loops(&explored_map);
+    println!("Result {}", res);
+    // assert!(detect_loops(&explored_map));
+
+    for i in 0..num_rows {
+        for j in 0..num_cols {
+            let mut explored_map = map.clone();
+            match &explored_map[i][j] {
+                Tile::Empty => {
+                    explored_map[i][j] = Tile::Obstacle;
+                    if detect_loops(&explored_map) {
+                        println!("Detected loop if inserted at {} {}", i, j);
+                        loops += 1;
+                    }
+                }
+
+                _ => (),
+            }
+        }
+    }
+    loops
 }
 
 pub fn solve_part_1(filepath: &str) -> u64 {
@@ -155,7 +306,7 @@ pub fn solve_part_1(filepath: &str) -> u64 {
     for tile_row in explored_map {
         for tile in tile_row {
             res += match tile {
-                Tile::Visited => 1,
+                Tile::Visited(_) => 1,
                 _ => 0,
             };
         }
@@ -166,11 +317,10 @@ pub fn solve_part_1(filepath: &str) -> u64 {
 
 pub fn solve_part_2(filepath: &str) -> u64 {
     let lines = load_lines(filepath);
-    let mut res = 0;
 
     let map = load_map(&lines);
 
-    res
+    find_loops(&map)
 }
 
 #[cfg(test)]
@@ -184,6 +334,6 @@ mod tests {
 
     #[test]
     fn test_example_part2() {
-        assert_eq!(solve_part_2("input_06_test"), 123);
+        assert_eq!(solve_part_2("input_06_test"), 6);
     }
 }
