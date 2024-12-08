bbb2bd58a9a61d515380349dea5bf8a1b757056c
Dariusz D <darekduda21@gmail.com>
2024-12-08 01:38:56 -0500

    feat(aoc_08): add soultion for part 2
    Signed-off-by: Dariusz D <darekduda21@gmail.com>

--- src/aoc_08.rs
+++ src/aoc_08.rs
@@ -46,7 +46,6 @@ fn point_in_boundaries(x: i64, y: i64, max_x: usize, max_y: usize) -> bool {
 
 pub fn solve_part_1(filepath: &str) -> u64 {
     let lines = load_lines(filepath);
-    let mut res = 0;
 
     let antenas = load_antenas(&lines);
 
@@ -101,7 +100,6 @@ pub fn solve_part_1(filepath: &str) -> u64 {
                         *next_antena_x as i64 + antena_diff_x,
                         next_antinode_y,
                     );
-                    res += 1;
                     antinodes.insert((
                         next_antena_x + antena_diff_x as usize,
                         next_antinode_y as usize,
@@ -127,32 +125,105 @@ pub fn solve_part_1(filepath: &str) -> u64 {
                         current_antena_x - antena_diff_x as usize,
                         current_antinode_y as usize,
                     ));
-                    res += 1;
                 }
             }
         }
     }
 
-    println!("{:?}", antinodes);
     println!("{}", antinodes.len());
-    for i in 0..num_rows {
-        for j in 0..num_cols {
-            match antinodes.contains(&(i, j)) {
-                true => print!("#"),
-                false => print!("."),
-            }
-        }
-        println!("");
-    }
 
     antinodes.len() as u64
 }
 
 pub fn solve_part_2(filepath: &str) -> u64 {
     let lines = load_lines(filepath);
-    let mut res = 0;
+    let antenas = load_antenas(&lines);
 
-    res
+    let mut antenas_locations: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
+
+    for (i, antennas_row) in antenas.iter().enumerate() {
+        for (j, antena) in antennas_row.iter().enumerate() {
+            match antena {
+                Tile::Antena(c) => {
+                    match &mut antenas_locations.get_mut(c) {
+                        Some(vec) => {
+                            vec.push((i, j));
+                        }
+                        None => {
+                            antenas_locations.insert(*c, vec![(i, j)]);
+                        }
+                    };
+                }
+                Tile::Empty => (),
+            }
+        }
+    }
+
+    let num_rows = antenas.len();
+    let num_cols = antenas[0].len();
+
+    let mut antinodes: HashSet<(usize, usize)> = HashSet::new();
+
+    for (antena_type, locations) in &antenas_locations {
+        println!("Analysing antenas {} {:?}", antena_type, locations);
+        for (i, location) in locations.iter().enumerate() {
+            let (current_antena_x, current_antena_y) = location;
+            for next_antena in &locations[i + 1..locations.len()] {
+                let (next_antena_x, next_antena_y) = next_antena;
+                println!("Analysing antenas {:?}", next_antena);
+                antinodes.insert((*current_antena_x, *current_antena_y));
+                antinodes.insert((*next_antena_x, *next_antena_y));
+
+                let antena_diff_x: i64 = (*current_antena_x as i64 - *next_antena_x as i64).abs();
+                let antena_diff_y: i64 = (*next_antena_y as i64 - *current_antena_y as i64).abs();
+                let mut next_antinode_x = *next_antena_x as i64 + antena_diff_x;
+                let mut next_antinode_y = if next_antena_y > current_antena_y {
+                    *next_antena_y as i64 + antena_diff_y
+                } else {
+                    *next_antena_y as i64 - antena_diff_y
+                };
+                while point_in_boundaries(next_antinode_x, next_antinode_y, num_rows, num_cols) {
+                    println!(
+                        "Analysing antinode {} {:?}",
+                        next_antinode_x, next_antinode_y,
+                    );
+                    antinodes.insert((next_antinode_x as usize, next_antinode_y as usize));
+                    if next_antena_y > current_antena_y {
+                        next_antinode_y += antena_diff_y
+                    } else {
+                        next_antinode_y -= antena_diff_y
+                    };
+                    next_antinode_x += antena_diff_x;
+                }
+                let mut current_antinode_x = *current_antena_x as i64 - antena_diff_x;
+                let mut current_antinode_y = if next_antena_y > current_antena_y {
+                    *current_antena_y as i64 - antena_diff_y
+                } else {
+                    *current_antena_y as i64 + antena_diff_y
+                };
+                while point_in_boundaries(
+                    current_antinode_x,
+                    current_antinode_y,
+                    num_rows,
+                    num_cols,
+                ) {
+                    println!(
+                        "Analysing antinode {} {:?}",
+                        current_antinode_x, current_antinode_y,
+                    );
+                    antinodes.insert((current_antinode_x as usize, current_antinode_y as usize));
+                    current_antinode_x -= antena_diff_x;
+                    if next_antena_y > current_antena_y {
+                        current_antinode_y -= antena_diff_y
+                    } else {
+                        current_antinode_y += antena_diff_y
+                    };
+                }
+            }
+        }
+    }
+
+    antinodes.len() as u64
 }
 
 #[cfg(test)]
@@ -166,6 +237,11 @@ mod tests {
 
     #[test]
     fn test_example_part2() {
-        assert_eq!(solve_part_2("input_08_test"), 11387);
+        assert_eq!(solve_part_2("input_08_test"), 34);
+    }
+
+    #[test]
+    fn test_example_part2_test_2() {
+        assert_eq!(solve_part_2("input_08_test2"), 9);
     }
 }
